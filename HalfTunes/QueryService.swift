078ee3ee031821e14edfd2eafba11b129dbd7251
Amit Kumar <amitsangwan898@gmail.com>
2018-03-27 15:46:37 +0530

    :point_right: Added new variable of URLSession and URLSessionDataTask and implemented getSearchResults method

--- HalfTunes/QueryService.swift
+++ HalfTunes/QueryService.swift
@@ -39,12 +39,40 @@ class QueryService {
   var tracks: [Track] = []
   var errorMessage = ""
 
-  // TODO
+  let defaultSession = URLSession(configuration: .default)
+  var dataTask: URLSessionDataTask?
 
+  
   func getSearchResults(searchTerm: String, completion: @escaping QueryResult) {
-    // TODO
-    DispatchQueue.main.async {
-      completion(self.tracks, self.errorMessage)
+
+    dataTask?.cancel()
+    
+    if var urlComponents = URLComponents(string: "https://itunes.apple.com/search")
+    {
+      urlComponents.query = "media=music&entity=song&term=\(searchTerm)"
+      
+      guard let url = urlComponents.url else
+      {
+        return
+      }
+      
+      dataTask = defaultSession.dataTask(with: url) { data , response , error in
+        defer { self.dataTask = nil }
+        
+        if let error = error
+        {
+           self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+        }
+        else if let data = data, let response = response as? HTTPURLResponse , response.statusCode == 200
+        {
+          self.updateSearchResults(data)
+          DispatchQueue.main.async {
+            completion(self.tracks,self.errorMessage)
+          }
+        }
+      }
+      
+      dataTask?.resume()
     }
   }
 
